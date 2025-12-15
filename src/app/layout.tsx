77af2604217e3e24e0b45bda0c66725113467c4a
thedogecraft <dantaealt@gmail.com>
2025-12-14 21:12:40 -0700

    fix: test some stuff

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -30,6 +30,32 @@ export default function RootLayout({ children }) {
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1565760898646999"
           crossOrigin="anonymous"
         ></Script>
+        <Script
+          async
+          src="https://fundingchoicesmessages.google.com/i/pub-1565760898646999?ers=1"
+          strategy="afterInteractive"
+        />
+
+        <Script id="google-funding-choices" strategy="afterInteractive">
+          {`
+            (function() {
+              function signalGooglefcPresent() {
+                if (!window.frames['googlefcPresent']) {
+                  if (document.body) {
+                    const iframe = document.createElement('iframe');
+                    iframe.style = 'width: 0; height: 0; border: none; z-index: -1000; left: -1000px; top: -1000px;';
+                    iframe.style.display = 'none';
+                    iframe.name = 'googlefcPresent';
+                    document.body.appendChild(iframe);
+                  } else {
+                    setTimeout(signalGooglefcPresent, 0);
+                  }
+                }
+              }
+              signalGooglefcPresent();
+            })();
+          `}
+        </Script>
       </head>
       <body
         className={`${poppins.className} antialiased min-h-screen flex flex-col overflow-x-hidden`}
